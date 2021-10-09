744ad8b969bbfa894e618bc46c726b999942ceb1
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-10-09 00:28:54 -0400

    Add files via upload

---
+++ W2HW2/W2HW2/Person1.java
@@ -0,0 +1,94 @@
+package W2HW2;
+import java.util.Scanner;
+public class Person1 extends Person {
+
+    private long ssn;
+    private long phone;
+
+public Person1() {
+
+}
+
+public Person1( String fName, String lName, int age, char gender, long ssn, long phone) {
+
+    super(fName, lName, age, gender);
+
+    this.ssn = ssn;
+    this.phone = phone;
+}
+public void scanner() {
+
+    Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter person first name: ");
+        input.nextLine();
+        fName = input.nextLine();
+
+        System.out.println("Enter person last name: ");
+        lName = input.nextLine();
+
+        System.out.println("Enter person age: ");
+        age = input.nextInt();
+
+        System.out.println("Enter person gender: ");
+        gender = input.next().charAt(0);
+
+        System.out.println("Enter person street: ");
+        input.nextLine();
+        street = input.nextLine();
+
+        System.out.println("Enter person city: ");
+        city = input.nextLine();
+
+        System.out.println("Enter person state: ");
+        state = input.nextLine();
+
+        System.out.println("Enter person zip: ");
+        zipcode = input.nextInt();
+
+        System.out.println("Enter person ssn: ");
+        ssn = input.nextInt();
+
+    System.out.println("Enter person ssn: ");
+    ssn = input.nextLong();
+
+    System.out.println("Enter person phone: ");
+    phone = input.nextLong();
+
+
+}
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn( long ssn ) {
+        this.ssn = ssn;
+    }
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone( long phone ) {
+        this.phone = phone;
+    }
+    public void display() {
+        super.display();
+        System.out.println("ssn:" + this.ssn);
+        System.out.println("phone:" + this.phone);
+    }
+//    public static void main(String[] args) {
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter person ssn: ");
+//        ssn = input.nextInt();
+//    }
+}
+//    public double calculate(int x) {
+//
+//        return 0;
+//
+//    }
+//
+//    public double compute(int y, int z) {
+//        return 0;
+//    }
---
+++ W2HW2/W2HW2/Student.java
@@ -0,0 +1,91 @@
+package W2HW2;
+import java.util.Scanner;
+
+public class Student extends Person implements PersonInterface{
+
+
+    int studNum = 1000;
+    String studMajor;
+    double subject1, subject2, subject3;
+
+    Student(){
+        studNum++;
+    }
+
+    Student( String fName, String lName, int age, char gender, int studNum, String studMajor, double subject1, double subject2, double subject3 ) {
+
+        super(fName, lName, age, gender);
+
+        this.studNum = studNum;
+        this.studMajor = studMajor;
+        this.subject1 = subject1;
+        this.subject2 = subject2;
+        this.subject3 = subject3;
+        studNum++;
+    }
+    public void scanner() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        fName = input.nextLine();
+
+        System.out.println("Enter student last name: ");
+        lName = input.nextLine();
+
+        System.out.println("Enter student age: ");
+        age = input.nextInt();
+
+        System.out.println("Enter student gender: ");
+        gender = input.next().charAt(0);
+
+        System.out.println("Enter student street: ");
+        input.nextLine();
+        street = input.nextLine();
+
+        System.out.println("Enter student city: ");
+        city = input.nextLine();
+
+        System.out.println("Enter student state: ");
+        state = input.nextLine();
+
+        System.out.println("Enter student zip: ");
+        zipcode = input.nextInt();
+
+        System.out.println("Enter student major: ");
+        input.nextLine();
+        studMajor = input.nextLine();
+
+		System.out.println("Enter student subject1 score: ");
+		subject1 = input.nextDouble();
+
+		System.out.println("Enter student subject2 score: ");
+		subject2 = input.nextDouble();
+
+		System.out.println("Enter student subject3 score: ");
+		subject3 = input.nextDouble();
+    }
+    public void display() {
+        super.display();
+        System.out.println("Student Number " + this.studNum);
+        System.out.println("Student Major " + this.studMajor);
+        System.out.println("Subject One Score " + this.subject1);
+        System.out.println("Subject Two Score " + this.subject2);
+        System.out.println("Subject Three Score " + this.subject3);
+        System.out.println(calculate(3));
+        System.out.println(compute(3, 4));
+    }
+
+    //	@Override
+    public double calculate(int totalSubjects) {
+        double average = ((this.subject1 + this.subject2 + this.subject3)/totalSubjects);
+        return average;
+
+    }
+
+    //	@Override
+    public double compute(int totalSubjects, int creditHours) {
+        double credits = totalSubjects * creditHours;
+        return credits;
+    }
+
+}
